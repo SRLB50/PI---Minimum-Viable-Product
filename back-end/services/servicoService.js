2828f8c6defe1adb789a6b25ac285a41ae3eeddc
JeanC777 <jecarlos7713@gmail.com>
2025-01-23 22:40:42 -0300

    feat: rota de exclusão do serviço

--- back-end/services/servicoService.js
+++ back-end/services/servicoService.js
@@ -26,8 +26,13 @@ class Service {
         })
     }
 
-    async remove(){
-
+    async remove(idEmpresa, idService){
+        return await models.Servico.destroy({
+            where: {
+                empresa_id  : idEmpresa,
+                id          : idService
+            }
+        })
     }
 
     update(){
@@ -70,8 +75,23 @@ const postService = async(request, reply) => {
     }
 }
 
+const removeService = async(request, reply) =>{
+    const {idService, idEmpresa} = request.query
+
+    const notEmptyData = idService != "" && idEmpresa != ""
+    if (!notEmptyData) {
+        reply.status(400).send({error: 'Envie o ID do usuário para buscar os serviços'})
+    }
+
+    const instance = new Service()
+    const dataRemove = await instance.remove(idEmpresa, idService)
+    console.log(dataRemove)
+    dataRemove == 1 ? reply.send({success: true, message: "Serviço removido com sucesso!"}) : reply.status(500).send({success: false, message: "Erro ao remover o serviço! Contate a equipe de suporte."})
+}
+
 module.exports ={
     getService,
     getServiceById,
-    postService
+    postService,
+    removeService
 }
