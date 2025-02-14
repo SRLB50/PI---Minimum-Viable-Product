1264868768d117863118c6b313d2ca30ec42d0c2
JeanC777 <jecarlos7713@gmail.com>
2025-02-14 20:49:53 -0300

    feat: inclusão da home como rota

--- front-end-side/navigation/tab-navigator.tsx
+++ front-end-side/navigation/tab-navigator.tsx
@@ -6,6 +6,7 @@ import { HeaderButton } from '../components/Template/HeaderButton';
 import { TabBarIcon } from '../components/Template/TabBarIcon';
 import One from '../screens/one';
 import Two from '../screens/two';
+import home from '~/screens/home';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,7 +18,7 @@ export default function TabLayout({ navigation }: Props) {
       screenOptions={{
         tabBarActiveTintColor: 'black',
       }}>
-      <Tab.Screen
+      {/* <Tab.Screen
         name="One"
         component={One}
         options={{
@@ -32,6 +33,14 @@ export default function TabLayout({ navigation }: Props) {
         options={{
           title: 'Tab Two',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      /> */}
+      <Tab.Screen
+        name='home'
+        component={home}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          headerShown: false
         }}
       />
     </Tab.Navigator>
