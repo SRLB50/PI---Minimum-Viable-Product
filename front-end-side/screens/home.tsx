411d8669159478c4fa7ca5b6c728072b57accfce
JeanC777 <jecarlos7713@gmail.com>
2025-02-14 20:51:00 -0300

    feat: home

---
+++ front-end-side/screens/home.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+const home = () => {
+  return (
+    <View className="flex-2 bg-white">
+        <View id="header" className="bg-black h-[100]">
+          <Text className="text-[]">Olá, José</Text>
+        </View>
+    </View>
+  )
+}
+
+export default home
