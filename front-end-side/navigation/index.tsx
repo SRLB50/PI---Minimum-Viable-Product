db333d4a072a748bef59a5bfff2ad91997f0f604
JeanC777 <jecarlos7713@gmail.com>
2025-02-14 20:49:20 -0300

    feat: inclusão de home como inicializador

--- front-end-side/navigation/index.tsx
+++ front-end-side/navigation/index.tsx
@@ -15,7 +15,7 @@ const Stack = createStackNavigator<RootStackParamList>();
 export default function RootStack() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="TabNavigator">
         <Stack.Screen
           name="Login"
           component={Login}
