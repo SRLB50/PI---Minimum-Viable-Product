5381da5140c54ddd4e6dd1b35589e59d898ee17f
JeanC777 <jecarlos7713@gmail.com>
2025-02-25 17:53:25 -0300

    feat: ajuste na busca da data atual

--- front-end-side/components/Home/Header.tsx
+++ front-end-side/components/Home/Header.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react"
 import { View, Text } from "react-native"
 
 type HeaderProps = {
@@ -7,26 +6,16 @@ type HeaderProps = {
 
 
 const Header = ({ name }: HeaderProps) => {
-    const months = [
-        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
-        "Julho", "Agosto", "Setembo", "Outubro", "Novembro", "Dezembro"
-    ]
-
-    const [date, setDate] = useState<string>("")
-
-    useEffect(() => {
-        const actualDate = new Date()
-
-        const dateString = `${actualDate.getDay()} de ${months[actualDate.getMonth()]} de ${actualDate.getFullYear()}`
-
-        setDate(dateString)
-    })
 
     return (
         <View id="header" className="bg-black flex-row items-end justify-around h-[130] py-[15] px-[10px] border-solid border-[#7FCCFF] border-b-[7px]">
             <View className="flex-row items-baseline justify-around w-full" >
                 <Text className="text-[#FFF] text-[1.8rem]">Olá, {name}</Text>
-                <Text className="text-[#FFF] text-[1rem]">{date}</Text>
+                <Text className="text-[#FFF] text-[1rem]">{new Intl.DateTimeFormat("pt-BR", {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric"
+                }).format(new Date())}</Text>
             </View>
         </View>
     )
