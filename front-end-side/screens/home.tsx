e20594dbc867a1d40fc43e7858e9f988f25f74fc
JeanC777 <jecarlos7713@gmail.com>
2025-02-25 18:04:36 -0300

    feat: ajuste na estrutura e criação do título de serviços

--- front-end-side/screens/home.tsx
+++ front-end-side/screens/home.tsx
@@ -7,10 +7,16 @@ const home = () => {
     <View className="container flex-1 bg-white">
       <Header name="Jean" />
 
-      <View className="p-[25px] flex-row justify-between">
-        <Dashboard value="4" description="Serviços para Hoje" bg="bg-[##7FCCFF]" />
-        
-        <Dashboard value="30" description="Atendidos no mês" bg="bg-[#2C2C2C]" />
+      <View className="p-[25px] ">
+        <View className="flex-row justify-between">
+          <Dashboard value="4" description="Serviços para Hoje" bg="bg-[##7FCCFF]" />
+          
+          <Dashboard value="30" description="Atendidos no mês" bg="bg-[#2C2C2C]" />
+        </View>
+
+        <View className="my-[25]">
+            <Text className="text-[1.8rem] font-medium text-[#2C2C2C]">Serviços</Text>
+        </View>
       </View>
     </View>
   )
