a5867ef83a7512f46e8d9836e26ea479ed543934
JeanC777 <jecarlos7713@gmail.com>
2025-03-09 01:17:33 -0300

    fix: ajuste na tab-navigator

--- front-end-side/navigation/tab-navigator.tsx
+++ front-end-side/navigation/tab-navigator.tsx
@@ -6,13 +6,26 @@ import { HeaderButton } from '../components/Template/HeaderButton';
 import { TabBarIcon } from '../components/Template/TabBarIcon';
 import home from '~/screens/home';
 import Profile from '~/screens/profile';
+import registerServices from '~/screens/registerServices';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from 'react';
 
 const Tab = createBottomTabNavigator();
 
 type Props = StackScreenProps<RootStackParamList, 'TabNavigator'>;
 
 export default function TabLayout({ navigation }: Props) {
-  
+  const [isCompany, setIsCompany] = useState<boolean | null>(null)
+
+  useEffect(() => {
+    const fetchCompanyData = async () => {
+      const empresa = await AsyncStorage.getItem('empresa');
+      setIsCompany(empresa == 'true');
+    };
+
+    fetchCompanyData();
+  }, []);
+
   return (
     <Tab.Navigator
       screenOptions={{
@@ -27,6 +40,19 @@ export default function TabLayout({ navigation }: Props) {
           tabBarShowLabel: false
         }}
       />
+      {
+        isCompany && (
+          <Tab.Screen
+            name='register-services'
+            component={registerServices}
+            options={{
+              tabBarIcon: ({ color }) => <TabBarIcon name="gear" color={color} />,
+              headerShown: false,
+              tabBarShowLabel: false
+            }}
+          />
+        )
+      }
       <Tab.Screen
         name='profile'
         component={Profile}
