097a40b900c8de7d688e121683f98dfd083deee2
JeanC777 <jecarlos7713@gmail.com>
2025-03-08 22:08:11 -0300

    feat: inclusão de chamada de todos os serviços

--- front-end-side/service/registerServices.ts
+++ front-end-side/service/registerServices.ts
@@ -1,4 +1,4 @@
-import { url } from "./configs/config.json"
+ import { url } from "./configs/config.json"
 
 type BodyServices  = {
     titulo     : string
@@ -84,6 +84,49 @@ class GetRegister{
             return data
         } catch (error) {
             alert(error)
+        }
+    }
+}
+
+class GetAllServices{
+    constructor(){}
+
+    #getRequest() {
+        let header = new Headers();
+        header.append("Content-Type", "application/json")
+
+        const requestOptions = {
+            method: 'GET',
+            headers: header
+        };
+
+        return requestOptions
+    }
+    async execute() {
+        try {
+            const requestOptions = this.#getRequest();
+
+            const response = await fetch(`${url}/services`, requestOptions);
+
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.status} - ${response.statusText}`);
+            }
+
+            const result = await response.json();
+            //console.log(result)
+            const data = {
+                success: true,
+                dataResult : result
+            };
+
+            return data;
+        } catch (error) {
+            console.error("Erro na execução:", error);
+            return {
+                success: false,
+                error: error,
+                dataResult : null
+            };
         }
     }
 }
@@ -192,5 +235,6 @@ export default {
     RegisterServices, 
     GetRegister,
     UpdateService,
-    RemoveService
+    RemoveService,
+    GetAllServices
 }
