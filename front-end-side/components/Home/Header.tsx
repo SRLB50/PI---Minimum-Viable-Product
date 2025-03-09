979c8065d2c9c25c0e0df9725ec13015ee8c4e10
JeanC777 <jecarlos7713@gmail.com>
2025-03-08 23:50:36 -0300

    feat: ajuste na props de nome

--- front-end-side/components/Home/Header.tsx
+++ front-end-side/components/Home/Header.tsx
@@ -11,8 +11,8 @@ const Header = ({name}: HeaderProps ) => {
     return (
         <View id="header" className="bg-black flex-row items-end justify-around h-[130] py-[15] px-[10px] border-solid border-[#7FCCFF] border-b-[7px]">
             <View className="flex-row items-baseline justify-around w-full" >
-                <Text className="text-[#FFF] text-[1.8rem]">Olá, {name}</Text>
-                <Text className="text-[#FFF] text-[1rem]">{new Intl.DateTimeFormat("pt-BR", {
+                <Text className="text-[#FFF] text-[1.8rem] w-[50%]">Olá, {name}</Text>
+                <Text className="text-[#FFF] text-[1rem] w-[50%]">{new Intl.DateTimeFormat("pt-BR", {
                     day: "2-digit",
                     month: "long",
                     year: "numeric"
